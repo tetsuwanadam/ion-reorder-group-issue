8d037b530d8863cb8fa4c0f8ce928fd4006ac35a
Adam Corn <adam@strike.co.jp>
2021-07-27 10:58:24 +0900

    Add issue code reproduction content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,101 @@
+<!--
+ion-item-group issue reproduction
+Issues:
+1. On touch devices, swiping up or down an item wrapped in ion-reorder immediately reorders the item, preventing page scroll
+2. ion-item-sliding doesn't work if it contains an item wrapped in ion-reorder (Item 3)
+-->
+
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header>
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Reorder</ion-title>
       </ion-toolbar>
     </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+
+    <ion-content id="content">
+      <ion-list>
+        <ion-reorder-group disabled="false">
+          <ion-reorder>
+            <ion-item>
+              <ion-label>
+                Item 1 (the whole item can be dragged)
+              </ion-label>
+            </ion-item>
+          </ion-reorder>
+
+          <ion-reorder>
+            <ion-item>
+              <ion-label>
+                Item 2 (the whole item can be dragged)
+              </ion-label>
+            </ion-item>
+          </ion-reorder>
+
+          <ion-item-sliding>
+            <ion-reorder>
+              <ion-item>
+                <ion-label>
+                  Item 3 (the whole item can be dragged, with ion-item-sliding)
+                </ion-label>
+              </ion-item>
+            </ion-reorder>
+
+            <ion-item-options side="start">
+              <ion-item-option>Favorite</ion-item-option>
+              <ion-item-option color="danger">Share</ion-item-option>
+            </ion-item-options>
+
+            <ion-item-options side="end">
+              <ion-item-option>Unread</ion-item-option>
+            </ion-item-options>
+          </ion-item-sliding>
+
+          <ion-reorder>
+            <ion-item>
+              <ion-label>
+                Item 4 (the whole item can be dragged)
+              </ion-label>
+            </ion-item>
+          </ion-reorder>
+
+          <ion-reorder>
+            <ion-item>
+              <ion-label>
+                Item 5 (the whole item can be dragged)
+              </ion-label>
+            </ion-item>
+          </ion-reorder>
+
+          <ion-reorder>
+            <ion-item>
+              <ion-label>
+                Item 6 (the whole item can be dragged)
+              </ion-label>
+            </ion-item>
+          </ion-reorder>
+
+          <ion-reorder>
+            <ion-item>
+              <ion-label>
+                Item 7 (the whole item can be dragged)
+              </ion-label>
+            </ion-item>
+          </ion-reorder>
+        </ion-reorder-group>
+      </ion-list>
+
+      <div style="height: 80vh; padding: 30px 15px; background: #eee;">
+        ion-reorder-group's disabled attribute is set to false.
+        <br><br>
+        The rest of this is filler content to make sure page is long enough to scroll.
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonReorder, IonReorderGroup, IonTitle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -31,38 +104,15 @@ export default defineComponent({
     IonContent,
     IonHeader,
     IonPage,
+    IonItem,
+    IonItemOption,
+    IonItemOptions,
+    IonItemSliding,
+    IonLabel,
+    IonReorder,
+    IonReorderGroup,
     IonTitle,
     IonToolbar
-  }
+  },
 });
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
